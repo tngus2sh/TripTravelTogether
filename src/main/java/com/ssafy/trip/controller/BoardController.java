309bab7f4214ff5b7ae36844214f1c70a1678311
lkm454545 <lkm454545@naver.com>
2023-05-14 23:05:07 +0900

    공지사항, 게시판 Controller Rest API 1차 생성

--- src/main/java/com/ssafy/trip/controller/BoardController.java
+++ src/main/java/com/ssafy/trip/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.ssafy.trip.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,12 +8,21 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -21,94 +31,218 @@ import com.ssafy.trip.board.model.service.BoardService;
 import com.ssafy.trip.user.model.dto.UserDto;
 import com.ssafy.trip.util.PageNavigation;
 
-@Controller
+@RestController
+//@Controller
 @RequestMapping("/board")
+@CrossOrigin("*")
 public class BoardController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class); 
 	
 	private BoardService boardService;
 
+	@Autowired
 	public BoardController(BoardService boardService) {
 		super();
 		this.boardService = boardService;
 	}
 	
-	@GetMapping("/list")
-	public ModelAndView list(@RequestParam Map<String, String> map) throws Exception {
-		logger.debug("board list map : {}", map);
-		ModelAndView mav = new ModelAndView();
-		List<BoardDto> list = boardService.listBoard(map);
-		PageNavigation pageNavigation = boardService.makePageNavigation(map);
-
-		logger.debug("board list list : {}", list);
-		mav.addObject("boards", list);
-		mav.addObject("navigation", pageNavigation);
-		mav.addObject("pgno", map.get("pgno"));
-		mav.addObject("key", map.get("key"));
-		mav.addObject("word", map.get("word"));
-		mav.setViewName("board/list");
-		return mav;
+//	@GetMapping("/list")
+//	public ModelAndView list(@RequestParam Map<String, String> map) throws Exception {
+//		logger.debug("board list map : {}", map);
+//		ModelAndView mav = new ModelAndView();
+//		List<BoardDto> list = boardService.listBoard(map);
+//		PageNavigation pageNavigation = boardService.makePageNavigation(map);
+//
+//		logger.debug("board list list : {}", list);
+//		mav.addObject("boards", list);
+//		mav.addObject("navigation", pageNavigation);
+//		mav.addObject("pgno", map.get("pgno"));
+//		mav.addObject("key", map.get("key"));
+//		mav.addObject("word", map.get("word"));
+//		mav.setViewName("board/list");
+//		return mav;
+//	}
+	
+	@GetMapping(value = "/list")
+	public ResponseEntity<?> list(@RequestBody Map<String, String> map) {
+		logger.debug("board list call : {}", map);
+		Map<String, Object> result = new HashMap<>();
+		try {
+			List<BoardDto> list = boardService.listBoard(map);
+			PageNavigation pageNavigation = boardService.makePageNavigation(map);
+			logger.debug("board list call : {}", map);
+			result.put("boardlist", list);
+			result.put("navigation", pageNavigation);
+			result.put("pgno", map.get("pgno"));
+			result.put("key", map.get("key"));
+			result.put("word", map.get("word"));
+			return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
 	}
 	
-	@GetMapping("/view")
-	public String view(@RequestParam("id") int id,  @RequestParam Map<String, String> map, Model model) throws Exception {
-		BoardDto boardDto = boardService.viewBoard(id);
-		boardService.updateHit(id);
-		model.addAttribute("board", boardDto);
-		model.addAttribute("pgno", map.get("pgno"));
-		model.addAttribute("key", map.get("key"));
-		model.addAttribute("word", map.get("word"));
-		return "/board/view";
+//	@GetMapping("/view")
+//	public String view(@RequestParam("id") int id,  @RequestParam Map<String, String> map, Model model) throws Exception {
+//		BoardDto boardDto = boardService.viewBoard(id);
+//		boardService.updateHit(id);
+//		model.addAttribute("board", boardDto);
+//		model.addAttribute("pgno", map.get("pgno"));
+//		model.addAttribute("key", map.get("key"));
+//		model.addAttribute("word", map.get("word"));
+//		return "/board/view";
+//	}
+	
+	@GetMapping(value = "/view")
+	public ResponseEntity<?> view(@RequestBody int id, Map<String, String> map){
+		logger.debug("board view id : {}", id);
+		Map<String, Object> result = new HashMap<>();
+		try {
+			boardService.updateHit(id);
+			BoardDto boardDto = boardService.viewBoard(id);
+			result.put("board", boardDto);
+			result.put("pgno", map.get("pgno"));
+			result.put("key", map.get("key"));
+			result.put("word", map.get("word"));
+			return new  ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
+		} catch(Exception e) {
+			return exceptionHandling(e);
+		}
 	}
 	
-	@GetMapping("/write")
-	public String write(@RequestParam Map<String, String> map, Model model) {
-		model.addAttribute("pgno", map.get("pgno"));
-		model.addAttribute("key", map.get("key"));
-		model.addAttribute("word", map.get("word"));
-		return "board/write";
+//	@GetMapping("/write")
+//	public String write(@RequestParam Map<String, String> map, Model model) {
+//		model.addAttribute("pgno", map.get("pgno"));
+//		model.addAttribute("key", map.get("key"));
+//		model.addAttribute("word", map.get("word"));
+//		return "board/write";
+//	}
+	
+	@GetMapping(value ="/write")
+	public ResponseEntity<?> write(@RequestBody Map<String, String> map) {
+		logger.debug("board write call ");
+		Map<String, Object> result = new HashMap<>();
+		try {
+			result.put("pgno", map.get("pgno"));
+			result.put("key", map.get("key"));
+			result.put("word", map.get("word"));
+			return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
 	}
 	
-	@PostMapping("/write")
-	public String write(BoardDto boardDto, HttpSession session, RedirectAttributes redirectAttributes) throws Exception {
-		UserDto userDto = (UserDto) session.getAttribute("userinfo");
-		boardDto.setUserId(userDto.getId());
+//	@PostMapping("/write")
+//	public String write(BoardDto boardDto, HttpSession session, RedirectAttributes redirectAttributes) throws Exception {
+//		UserDto userDto = (UserDto) session.getAttribute("userinfo");
+//		boardDto.setUserId(userDto.getUserid());
+//		
+//		boardService.writeBoard(boardDto);
+//		redirectAttributes.addAttribute("pgno", "1");
+//		redirectAttributes.addAttribute("key", "");
+//		redirectAttributes.addAttribute("word", "");
+//		return "redirect:/board/list";
+//	}
+	
+	@PostMapping(value = "/write")
+	public ResponseEntity<?> write(BoardDto boardDto, HttpSession session){
+		logger.debug("board write call : {} ", boardDto);
+		Map<String, Object> result = new HashMap<>();
+		try {
+			UserDto userDto = (UserDto) session.getAttribute("userinfo");
+			boardDto.setUserId(userDto.getUserid());
+			
+			boardService.writeBoard(boardDto);
+			result.put("pgno",1);
+			result.put("key", 1);
+			result.put("word", 1);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+	}
+	
+//	@GetMapping("/modify")
+//	public String modify(@RequestParam("id") int id, HttpSession session, @RequestParam Map<String, String> map, Model model) throws Exception {
+//		BoardDto boardDto = boardService.viewBoard(id);
+//		model.addAttribute("board", boardDto);
+//		model.addAttribute("pgno", map.get("pgno"));
+//		model.addAttribute("key", map.get("key"));
+//		model.addAttribute("word", map.get("word"));
+//		return "board/modify";
+//	}
+	
+	@GetMapping(value = "/modify")
+	public ResponseEntity<?> modify(@RequestBody int id, HttpSession session, @RequestBody Map<String, String> map){
+		logger.debug("board modify call {} ", id);
+		Map<String, Object> result = new HashMap<>();
 		
-		boardService.writeBoard(boardDto);
-		redirectAttributes.addAttribute("pgno", "1");
-		redirectAttributes.addAttribute("key", "");
-		redirectAttributes.addAttribute("word", "");
-		return "redirect:/board/list";
+		try {
+			BoardDto boardDto = boardService.viewBoard(id);
+			result.put("boardDto", boardDto);
+			result.put("pgno", map.get("pgno"));
+			result.put("key", map.get("key"));
+			result.put("word", map.get("word"));
+			
+			return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
+		} catch(Exception e) {
+			return exceptionHandling(e);
+		}
 	}
 	
-	@GetMapping("/modify")
-	public String modify(@RequestParam("id") int id, HttpSession session, @RequestParam Map<String, String> map, Model model) throws Exception {
-		BoardDto boardDto = boardService.viewBoard(id);
-		model.addAttribute("board", boardDto);
-		model.addAttribute("pgno", map.get("pgno"));
-		model.addAttribute("key", map.get("key"));
-		model.addAttribute("word", map.get("word"));
-		return "board/modify";
+//	@PostMapping("/modify")
+//	public String modify(BoardDto boardDto, @RequestParam Map<String, String> map,
+//			RedirectAttributes redirectAttributes) throws Exception {
+//		boardService.modifyBoard(boardDto);
+//		redirectAttributes.addAttribute("pgno", map.get("pgno"));
+//		redirectAttributes.addAttribute("key", map.get("key"));
+//		redirectAttributes.addAttribute("word", map.get("word"));
+//		return "redirect:/board/list";
+//	}
+	
+	@PutMapping(value="/modify")
+	public ResponseEntity<?> modify(BoardDto boardDto, @RequestBody Map<String,String> map){
+		logger.debug("modify board : {}", boardDto);
+		Map<String, Object> result = new HashMap<>();
+		try {
+			boardService.modifyBoard(boardDto);
+			result.put("pgno",1);
+			result.put("key", 1);
+			result.put("word", 1);
+			return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
 	}
 	
-	@PostMapping("/modify")
-	public String modify(BoardDto boardDto, @RequestParam Map<String, String> map,
-			RedirectAttributes redirectAttributes) throws Exception {
-		boardService.modifyBoard(boardDto);
-		redirectAttributes.addAttribute("pgno", map.get("pgno"));
-		redirectAttributes.addAttribute("key", map.get("key"));
-		redirectAttributes.addAttribute("word", map.get("word"));
-		return "redirect:/board/list";
+//	@GetMapping("/delete")
+//	public String delete(@RequestParam("id") int id, @RequestParam Map<String, String> map,
+//			RedirectAttributes redirectAttributes) throws Exception {
+//		boardService.deleteBoard(id);
+//		redirectAttributes.addAttribute("pgno", map.get("pgno"));
+//		redirectAttributes.addAttribute("key", map.get("key"));
+//		redirectAttributes.addAttribute("word", map.get("word"));
+//		return "redirect:/board/list";
+//	}
+	
+	@DeleteMapping(value = "/delete")
+	public ResponseEntity<?> delete(@RequestBody int id, @RequestBody Map<String,String> map){
+		logger.debug("delete board : {}", id);
+		Map<String, Object> result = new HashMap<>();
+		try {
+			boardService.deleteBoard(id);
+			result.put("pgno",1);
+			result.put("key", 1);
+			result.put("word", 1);
+			return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
 	}
 	
-	@GetMapping("/delete")
-	public String delete(@RequestParam("id") int id, @RequestParam Map<String, String> map,
-			RedirectAttributes redirectAttributes) throws Exception {
-		boardService.deleteBoard(id);
-		redirectAttributes.addAttribute("pgno", map.get("pgno"));
-		redirectAttributes.addAttribute("key", map.get("key"));
-		redirectAttributes.addAttribute("word", map.get("word"));
-		return "redirect:/board/list";
+	private ResponseEntity<String> exceptionHandling(Exception e) {
+		e.printStackTrace();
+		return new ResponseEntity<String>("Error : " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
--- src/main/java/com/ssafy/trip/controller/NotificationController.java
+++ src/main/java/com/ssafy/trip/controller/NotificationController.java
@@ -4,12 +4,22 @@ import java.util.*;
 
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -18,89 +28,215 @@ import com.ssafy.trip.notification.model.service.NotificationService;
 import com.ssafy.trip.user.model.dto.UserDto;
 import com.ssafy.trip.util.PageNavigation;
 
-@Controller
+@RestController
+//@Controller
 @RequestMapping("/notification")
+@CrossOrigin("*")
 public class NotificationController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(NotificationController.class); 
 	
 	private NotificationService notificationService;
 
+	@Autowired
 	public NotificationController(NotificationService notificationService) {
 		super();
 		this.notificationService = notificationService;
 	}
 	
-	@GetMapping("/list")
-	public ModelAndView list(@RequestParam Map<String, String> map) throws Exception {
-		ModelAndView mav = new ModelAndView();
-		List<NotificationDto> list = notificationService.listNotification(map);
-		PageNavigation pageNavigation = notificationService.makePageNavigation(map);
+//	@GetMapping("/list")
+//	public ModelAndView list(@RequestParam Map<String, String> map) throws Exception {
+//		ModelAndView mav = new ModelAndView();
+//		List<NotificationDto> list = notificationService.listNotification(map);
+//		PageNavigation pageNavigation = notificationService.makePageNavigation(map);
+//		
+//		mav.addObject("notifications", list);
+//		mav.addObject("navigation", pageNavigation);
+//		mav.addObject("pgno", map.get("pgno"));
+//		mav.addObject("key", map.get("key"));
+//		mav.addObject("word", map.get("word"));
+//		mav.setViewName("notification/list");
+//		return mav;
+//	}
+	
+	@GetMapping(value = "/list")
+	public ResponseEntity<?> list(@RequestBody Map<String, String> map) {
+		logger.debug("notification list call : {}", map);
+		Map<String, Object> result = new HashMap<>();
+		try {
+			List<NotificationDto> list = notificationService.listNotification(map);
+			PageNavigation pageNavigation = notificationService.makePageNavigation(map);
+			result.put("notification", list);
+			result.put("navigation", pageNavigation);
+			result.put("pgno", map.get("pgno"));
+			result.put("key", map.get("key"));
+			result.put("word", map.get("word"));
+			return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+	}
+	
+//	@GetMapping("/view")
+//	public String view(@RequestParam("id") int id, @RequestParam Map<String, String> map, Model model) throws Exception {
+//		NotificationDto notificationDto = notificationService.viewNotification(id);
+//		notificationService.updateHit(id);
+//		model.addAttribute("notification", notificationDto);
+//		model.addAttribute("pgno", map.get("pgno"));
+//		model.addAttribute("key", map.get("key"));
+//		model.addAttribute("word", map.get("word"));
+//		return "/notification/view";
+//	}
+	
+	@GetMapping(value = "/view")
+	public ResponseEntity<?> view(@RequestBody int id, Map<String, String> map){
+		logger.debug("notification view id : {}", id);
+		Map<String, Object> result = new HashMap<>();
+		try {
+			notificationService.updateHit(id);
+			NotificationDto notificationDto = notificationService.viewNotification(id);
+			result.put("notification", notificationDto);
+			result.put("pgno", map.get("pgno"));
+			result.put("key", map.get("key"));
+			result.put("word", map.get("word"));
+			return new  ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
+		} catch(Exception e) {
+			return exceptionHandling(e);
+		}
+	}
+	
+//	@GetMapping("/write")
+//	public String write(@RequestParam Map<String, String> map, Model model) {
+//		model.addAttribute("pgno", map.get("pgno"));
+//		model.addAttribute("key", map.get("key"));
+//		model.addAttribute("word", map.get("word"));
+//		return "notification/write";
+//	}
+	
+	@GetMapping(value ="/write")
+	public ResponseEntity<?> write(@RequestBody Map<String, String> map) {
+		logger.debug("notification write call ");
+		Map<String, Object> result = new HashMap<>();
+		try {
+			result.put("pgno", map.get("pgno"));
+			result.put("key", map.get("key"));
+			result.put("word", map.get("word"));
+			return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+	}
+	
+//	@PostMapping("/write")
+//	public String write(NotificationDto notificationDto, HttpSession session, RedirectAttributes redirectAttributes) throws Exception {
+//		UserDto userDto = (UserDto) session.getAttribute("userinfo");
+//		notificationDto.setUserId(userDto.getUserid());
+//		
+//		notificationService.writeNotification(notificationDto);
+//		redirectAttributes.addAttribute("pgno", "1");
+//		redirectAttributes.addAttribute("key", "");
+//		redirectAttributes.addAttribute("word", "");
+//		return "redirect:/notification/list";
+//	}
+	
+	@PostMapping(value = "/write")
+	public ResponseEntity<?> write(NotificationDto notificationDto, HttpSession session){
+		logger.debug("notification write call : {} ", notificationDto);
+		Map<String, Object> result = new HashMap<>();
+		try {
+			UserDto userDto = (UserDto) session.getAttribute("userinfo");
+			notificationDto.setUserId(userDto.getUserid());
+			
+			notificationService.writeNotification(notificationDto);
+			result.put("pgno",1);
+			result.put("key", 1);
+			result.put("word", 1);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+	}
+	
+//	@GetMapping("/modify")
+//	public String modify(@RequestParam("id") int id, HttpSession session, @RequestParam Map<String, String> map, Model model) throws Exception {
+//		NotificationDto notificationDto = notificationService.viewNotification(id);
+//		model.addAttribute("notification", notificationDto);
+//		model.addAttribute("pgno", map.get("pgno"));
+//		model.addAttribute("key", map.get("key"));
+//		model.addAttribute("word", map.get("word"));
+//		return "notification/modify";
+//	}
+	
+	@GetMapping(value = "/modify")
+	public ResponseEntity<?> modify(@RequestBody int id, HttpSession session, @RequestBody Map<String, String> map){
+		logger.debug("notification modify call {} ", id);
+		Map<String, Object> result = new HashMap<>();
 		
-		mav.addObject("notifications", list);
-		mav.addObject("navigation", pageNavigation);
-		mav.addObject("pgno", map.get("pgno"));
-		mav.addObject("key", map.get("key"));
-		mav.addObject("word", map.get("word"));
-		mav.setViewName("notification/list");
-		return mav;
+		try {
+			NotificationDto notificationDto = notificationService.viewNotification(id);
+			result.put("notificationDto", notificationDto);
+			result.put("pgno", map.get("pgno"));
+			result.put("key", map.get("key"));
+			result.put("word", map.get("word"));
+			
+			return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
+		} catch(Exception e) {
+			return exceptionHandling(e);
+		}
 	}
 	
-	@GetMapping("/view")
-	public String view(@RequestParam("id") int id, @RequestParam Map<String, String> map, Model model) throws Exception {
-		NotificationDto notificationDto = notificationService.viewNotification(id);
-		notificationService.updateHit(id);
-		model.addAttribute("notification", notificationDto);
-		model.addAttribute("pgno", map.get("pgno"));
-		model.addAttribute("key", map.get("key"));
-		model.addAttribute("word", map.get("word"));
-		return "/notification/view";
-	}
-	@GetMapping("/write")
-	public String write(@RequestParam Map<String, String> map, Model model) {
-		model.addAttribute("pgno", map.get("pgno"));
-		model.addAttribute("key", map.get("key"));
-		model.addAttribute("word", map.get("word"));
-		return "notification/write";
+//	@PostMapping("/modify")
+//	public String modify(NotificationDto notificationDto, @RequestParam Map<String, String> map,
+//			RedirectAttributes redirectAttributes) throws Exception {
+//		notificationService.modifyNotification(notificationDto);
+//		redirectAttributes.addAttribute("pgno", map.get("pgno"));
+//		redirectAttributes.addAttribute("key", map.get("key"));
+//		redirectAttributes.addAttribute("word", map.get("word"));
+//		return "redirect:/notification/list";
+//	}
+	
+	@PutMapping(value="/modify")
+	public ResponseEntity<?> modify(NotificationDto notificationDto, @RequestBody Map<String,String> map){
+		logger.debug("modify notification : {}", notificationDto);
+		Map<String, Object> result = new HashMap<>();
+		try {
+			notificationService.modifyNotification(notificationDto);
+			result.put("pgno",1);
+			result.put("key", 1);
+			result.put("word", 1);
+			return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
 	}
 	
-	@PostMapping("/write")
-	public String write(NotificationDto notificationDto, HttpSession session, RedirectAttributes redirectAttributes) throws Exception {
-		UserDto userDto = (UserDto) session.getAttribute("userinfo");
-		notificationDto.setUserId(userDto.getId());
-		
-		notificationService.writeNotification(notificationDto);
-		redirectAttributes.addAttribute("pgno", "1");
-		redirectAttributes.addAttribute("key", "");
-		redirectAttributes.addAttribute("word", "");
-		return "redirect:/notification/list";
+//	@GetMapping("/delete")
+//	public String delete(@RequestParam("id") int id, @RequestParam Map<String, String> map,
+//			RedirectAttributes redirectAttributes) throws Exception {
+//		notificationService.deleteNotification(id);
+//		redirectAttributes.addAttribute("pgno", map.get("pgno"));
+//		redirectAttributes.addAttribute("key", map.get("key"));
+//		redirectAttributes.addAttribute("word", map.get("word"));
+//		return "redirect:/notification/list";
+//	}
+	
+	@DeleteMapping(value = "/delete")
+	public ResponseEntity<?> delete(@RequestBody int id, @RequestBody Map<String,String> map){
+		logger.debug("delete notification : {}", id);
+		Map<String, Object> result = new HashMap<>();
+		try {
+			notificationService.deleteNotification(id);
+			result.put("pgno",1);
+			result.put("key", 1);
+			result.put("word", 1);
+			return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
 	}
 	
-	@GetMapping("/modify")
-	public String modify(@RequestParam("id") int id, HttpSession session, @RequestParam Map<String, String> map, Model model) throws Exception {
-		NotificationDto notificationDto = notificationService.viewNotification(id);
-		model.addAttribute("notification", notificationDto);
-		model.addAttribute("pgno", map.get("pgno"));
-		model.addAttribute("key", map.get("key"));
-		model.addAttribute("word", map.get("word"));
-		return "notification/modify";
-	}
-	
-	@PostMapping("/modify")
-	public String modify(NotificationDto notificationDto, @RequestParam Map<String, String> map,
-			RedirectAttributes redirectAttributes) throws Exception {
-		notificationService.modifyNotification(notificationDto);
-		redirectAttributes.addAttribute("pgno", map.get("pgno"));
-		redirectAttributes.addAttribute("key", map.get("key"));
-		redirectAttributes.addAttribute("word", map.get("word"));
-		return "redirect:/notification/list";
-	}
-	
-	@GetMapping("/delete")
-	public String delete(@RequestParam("id") int id, @RequestParam Map<String, String> map,
-			RedirectAttributes redirectAttributes) throws Exception {
-		notificationService.deleteNotification(id);
-		redirectAttributes.addAttribute("pgno", map.get("pgno"));
-		redirectAttributes.addAttribute("key", map.get("key"));
-		redirectAttributes.addAttribute("word", map.get("word"));
-		return "redirect:/notification/list";
+	private ResponseEntity<String> exceptionHandling(Exception e) {
+		e.printStackTrace();
+		return new ResponseEntity<String>("Error : " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
