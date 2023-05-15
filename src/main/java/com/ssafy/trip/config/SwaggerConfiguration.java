d311899507fd92f3d0fc89aeeefc2d08312552d9
lkm454545 <lkm454545@naver.com>
2023-05-15 17:01:30 +0900

    Refactor: swagger configuration 업데이트

--- src/main/java/com/ssafy/trip/config/SwaggerConfiguration.java
+++ src/main/java/com/ssafy/trip/config/SwaggerConfiguration.java
@@ -1,63 +1,94 @@
 package com.ssafy.trip.config;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
 @EnableSwagger2
+@Configuration
+@SuppressWarnings("unchecked") // warning 제거
 public class SwaggerConfiguration {
 
-//	Swagger-UI 2.x 확인
-//	http://localhost:8080/{your-app-root}/swagger-ui.html
-//	Swagger-UI 3.x 확인
-//	http://localhost:8080/{your-app-root}/swagger-ui/index.html
+//    Swagger-UI 2.x 확인
+//    http://localhost[:8080]/{your-app-root}/swagger-ui.html
+//    Swagger-UI 3.x 확인
+//    http://localhost[:8080]/{your-app-root}/swagger-ui/index.html
 
-	private String version = "V1";
-	private String title = "SSAFY Board API " + version;
-	
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).consumes(getConsumeContentTypes()).produces(getProduceContentTypes())
-					.apiInfo(apiInfo()).groupName(version).select()
-					.apis(RequestHandlerSelectors.basePackage("com.ssafy.admin.controller"))
-					.paths(regex("/admin/.*")).build()
-					.useDefaultResponseMessages(false);
-	}
-	
-	private Set<String> getConsumeContentTypes() {
-        Set<String> consumes = new HashSet<>();
-        consumes.add("application/json;charset=UTF-8");
-//      consumes.add("application/xml;charset=UTF-8");
-        consumes.add("application/x-www-form-urlencoded");
-        return consumes;
+    private String version = "V1";
+    private String title = "SSAFY Board-Vuejs API " + version;
+
+    private ApiInfo apiInfo() {
+        String descript = "SSAFY Vuejs API Reference for Developers<br>";
+        descript += "<img src=\"http://localhost:9999/vue/static/assets/img/ssafy_logo.png\">";
+        return new ApiInfoBuilder().title(title).description(descript)
+//                .termsOfServiceUrl("https://edu.ssafy.com")
+                .contact(new Contact("SSAFY", "https://edu.ssafy.com", "ssafy@ssafy.com")).license("SSAFY License")
+                .licenseUrl("ssafy@ssafy.com").version("1.0").build();
     }
 
-    private Set<String> getProduceContentTypes() {
-        Set<String> produces = new HashSet<>();
-        produces.add("application/json;charset=UTF-8");
-        return produces;
+    // API마다 구분짓기 위한 설정.
+    @Bean
+    public Docket userApi() {
+        return getDocket("회원", Predicates.or(PathSelectors.regex("/user.*")));
+    }
+    
+    @Bean
+    public Docket boardApi() {
+        return getDocket("게시판", Predicates.or(PathSelectors.regex("/board.*")));
+    }
+    
+    @Bean
+    public Docket noticeApi() {
+        return getDocket("공지사항", Predicates.or(PathSelectors.regex("/notification.*")));
+    }
+    
+    @Bean
+    public Docket touristApi() {
+        return getDocket("지역별 검색", Predicates.or(PathSelectors.regex("/attraction.*")));
+    }
+    
+    @Bean
+    public Docket planApi() {
+        return getDocket("여행계획", Predicates.or(PathSelectors.regex("/plan.*")));
     }
 
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title(title)
-				.description("<h3>SSAFY API Reference for Developers</h3>Swagger를 이용한 Board API<br><img src=\"/assets/img/ssafy_logo.png\" width=\"150\">") 
-				.contact(new Contact("SSAFY", "https://edu.ssafy.com", "ssafy@ssafy.com"))
-				.license("SSAFY License")
-				.licenseUrl("https://www.ssafy.com/ksp/jsp/swp/etc/swpPrivacy.jsp")
-				.version("1.0").build();
-	}
-	
+    @Bean
+    public Docket hotplaceApi() {
+        return getDocket("핫플레이스", Predicates.or(PathSelectors.regex("/hotplace.*")));
+    }
+    
+    public Docket getDocket(String groupName, Predicate<String> predicate) {
+//        List<ResponseMessage> responseMessages = new ArrayList<ResponseMessage>();
+//        responseMessages.add(new ResponseMessageBuilder().code(200).message("OK !!!").build());
+//        responseMessages.add(new ResponseMessageBuilder().code(500).message("서버 문제 발생 !!!").responseModel(new ModelRef("Error")).build());
+//        responseMessages.add(new ResponseMessageBuilder().code(404).message("페이지를 찾을 수 없습니다 !!!").build());
+        return new Docket(DocumentationType.SWAGGER_2).groupName(groupName).apiInfo(apiInfo()).select()
+//                .apis(RequestHandlerSelectors.basePackage("com.ssafy.user.controller"))
+//                .paths(predicate)
+                .apis(RequestHandlerSelectors.any())
+                .paths(predicate)
+                .build();
+//                .useDefaultResponseMessages(false)
+//                .globalResponseMessage(RequestMethod.GET,responseMessages);
+    }
+
+    // swagger ui 설정.
+    @Bean
+    public UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder().displayRequestDuration(true).validatorUrl("").build();
+    }
+
 }
