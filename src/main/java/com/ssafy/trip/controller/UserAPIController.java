35003e5af5f778fb27483527844d1061b84b07d9
tngus2sh <ssuhyeon98@gmail.com>
2023-05-17 22:35:49 +0900

    Refactor: router>index.js beforeEnter 추가

--- src/main/java/com/ssafy/trip/controller/UserAPIController.java
+++ src/main/java/com/ssafy/trip/controller/UserAPIController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,7 @@ import io.swagger.annotations.ApiParam;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = { "*" })
 @Api("사용자 컨트롤러 API V1")
 public class UserAPIController {
 	
